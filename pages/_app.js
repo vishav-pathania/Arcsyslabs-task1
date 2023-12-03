4caad69d59f369b02c7c02f3f157e32d05715e56
vishav-pathania <vishavpathania40@gmail.com>
2023-12-03 13:13:45 +0530

    Setup eslint, prettier, tailwind, fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Open_Sans, Poppins } from "next/font/google";
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  display: "swap",
+  variable: "--font-open-sans",
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
+  variable: "--font-poppins",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${openSans.variable} ${poppins.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
